5dcdf6e66666f3c26a5701ea36886c741d161d94
Luqman Luke <luqmanluke19@gmail.com>
2021-06-02 04:39:01 +0800

    Splash Screen with anim

---
+++ app/src/main/java/com/capstone/rangkayo/SplashScreen.kt
@@ -0,0 +1,38 @@
+package com.capstone.rangkayo
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.capstone.rangkayo.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+
+    companion object {
+        const val INTERVAL = 2000
+    }
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val anim = AnimationUtils.loadAnimation(applicationContext, R.anim.fade_in)
+        val animScale = AnimationUtils.loadAnimation(applicationContext, R.anim.scale)
+        binding.image.startAnimation(anim)
+        binding.image.startAnimation(animScale)
+
+
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }, INTERVAL.toLong())
+    }
+
+}
