c33d7e6451f6dee7cd6f3380b5608be19ae5d195
Luqman Luke <luqmanluke19@gmail.com>
2021-06-01 19:49:03 +0800

    changed

--- app/src/main/java/com/capstone/rangkayo/MainActivity.kt
+++ app/src/main/java/com/capstone/rangkayo/MainActivity.kt
@@ -2,12 +2,13 @@ package com.capstone.rangkayo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val string = "Rangkayo"
+
     }
 }
